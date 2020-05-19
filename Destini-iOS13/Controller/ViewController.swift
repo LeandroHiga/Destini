ee0e0c3fa3229bd524514bd6487edb153c4cece9
Leandro Higa <lean_higa@hotmail.com>
2020-05-18 23:41:01 -0300

    Destini Game

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Connect buttons and label
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    // Create new story
+    var story = StoryBrain()
+    
+    // User pick choice
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle! // Choice 1 or Choice 2
+        story.nextStory(userChoice)
+
+        updateUI()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        choice1Button.titleLabel?.adjustsFontSizeToFitWidth = true
+        choice2Button.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        updateUI()
+        
     }
 
+    func updateUI() {
+        // Starting labels
+        storyLabel.text = story.getTitle()
+        choice1Button.setTitle(story.getChoice1(), for: .normal)
+        choice2Button.setTitle(story.getChoice2(), for: .normal)
+    }
 
 }
 
